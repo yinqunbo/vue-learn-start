ed42b0f35ecee19e9e04c05ea0435dcb8e23698b
yinqunbo <yinqunbo@vip.qq.com>
2020-09-16 16:58:13 +0800

    Child-to-Father-emit

---
+++ src/components/1.Child-to-Father-emit/Child.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>我是儿子组件</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: "我是儿子"
+            }
+        },
+        mounted() {
+            this.$emit("say",this.name)
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/1.Child-to-Father-emit/Father.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Child @say="say"/>
+        {{word}}
+    </div>
+</template>
+
+<script>
+import Child from "../../components/1.Child-to-Father-emit/Child.vue"
+export default {
+    data() {
+        return {
+            word: ""
+        }
+    },
+    components: {
+        Child
+    },
+    methods: {
+        say(data){
+            this.word = data
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
